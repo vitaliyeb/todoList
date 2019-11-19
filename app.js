e874af26c4a8533b2605fffa0dc4b434c7b61bae
Vitaliy_B <vlvlv.vovov@bk.ru>
2019-11-19 15:45:29 +0500

    почти готово

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const router = function() {
             return URL.parse(request.url).pathname;
         },
         retPage: function(request) {
+            try {
+                fs.statSync(`./dist${this.countPath(request)}.html`);
+            }
+            catch (err) {
+                return dynamically.retContent(fs.readFileSync(`./dist/notPage.html`, "utf8"));
+            }
             return dynamically.retContent(fs.readFileSync(`./dist${this.countPath(request)}.html`, "utf8"));
         }
     }
@@ -16,7 +22,8 @@ const router = function() {
 const VitDB = function () {
     let createDayTusk =  function (obj) {
         this.date = obj.currentDate;
-        this.tusk = ["obj.tusk"];
+        this.tusk = [obj.newTusk];
+        this.done = false;
     }
     return {
         readVDB: function (){
@@ -26,7 +33,7 @@ const VitDB = function () {
             if(req.method === "GET"){
                 let arrVDB = JSON.parse(this.readVDB());
                 let currentItem = arrVDB.find((item)=> item.date == req.headers.getdate);
-                if(!!currentItem){
+                if(!!currentItem && !!currentItem.tusk.length){
                     return this.retResponse(res, JSON.stringify(currentItem))
                 }
                     return this.retResponse(res);
@@ -38,18 +45,28 @@ const VitDB = function () {
                 req.on('end', ()=>{
                     data = JSON.parse(data);
                     let arrVDB = Array.from(JSON.parse(this.readVDB()));
+
                     let currentItem = arrVDB.find((item)=> item.date == data.currentDate);
-                    if(!currentItem){
-                        arrVDB.push(new createDayTusk(data));
-                    }else {
-                        currentItem.tusk.push(data.newTusk);
+
+                    if(data.do == 'isDone'){
+                            currentItem.tusk[data.id].done = data.done;
+                     }else if(data.do == 'delete'){
+                            currentItem.tusk.splice(data.id, 1);
+                    }else if(data.do == 'clear'){
+                        arrVDB.splice(arrVDB.indexOf(currentItem), 1);
+                    }else{
+                        if(!currentItem){
+                            arrVDB.push(new createDayTusk(data));
+                        }else {
+                            currentItem.tusk.push(data.newTusk);
+                        }
                     }
                     fs.writeFileSync(`./dist/VitDateBase.txt`, JSON.stringify(arrVDB));
                     this.retResponse(res);
                 });
             }
         },
-        retResponse: function (response, data = '{"tusk":["У вас нет задач на сегодня", "У вас нет задач на сегодня"]}') {
+        retResponse: function (response, data = '{"tusk":[{"text":"У вас нет задач на этот день", "defaulted": true}]}') {
             response.write(data);
             response.end();
         }
@@ -76,11 +93,8 @@ const server = http.createServer((request, response)=>{
         response.end();
     }else {
         VitDB.DBInit(request, response);
+}});
+
+server.listen(8080, '127.168.0.1', () => {
+    console.log(`Server running at http://127.168.0.1:8080/index`);
 });
-
-server.listen({
-    port: 8080,
-    host: '127.168.0.1'
-})
-
-
